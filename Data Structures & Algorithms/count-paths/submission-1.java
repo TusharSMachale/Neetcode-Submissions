7ff822c22a9e0599e5b58288be8def6330fb7afc
Tushar S Machale <110717519+TusharSMachale@users.noreply.github.com>
2026-06-03 20:13:20 +0530

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int total = m + n - 2;
+        double ans = 1;
+        int r = Math.min(m-1, n-1);
+
+        for(int i=1; i<=r; i++){
+            ans = ans * (total - r + i)/i;
+        }
+        return (int)ans;
+    }
+}
