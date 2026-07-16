0a0f52dccf745690e9fdd779ddce1eeef8bc2166
Tushar S Machale <110717519+TusharSMachale@users.noreply.github.com>
2026-07-16 15:33:35 +0530

    Add: binary-search-tree-iterator - submission-0

---
+++ Data Structures & Algorithms/binary-search-tree-iterator/submission-0.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+    private Stack<TreeNode> st = new Stack<>();
+    public BSTIterator(TreeNode root) {
+        pushAll(root);
+    }
+    
+    public int next() {
+        TreeNode temp = st.pop();
+        if(temp.right != null){
+            pushAll(temp.right);
+        }
+        return temp.val;
+    }
+    
+    public boolean hasNext() {
+        return !st.isEmpty();
+    }
+
+    public void pushAll(TreeNode root){
+        if(root == null) return;
+
+        st.push(root);
+        while(root.left != null){
+            st.push(root.left);
+            root = root.left;
+        }
+        return;
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
