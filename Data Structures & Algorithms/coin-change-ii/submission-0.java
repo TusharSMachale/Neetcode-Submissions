8616ee9b4be7ce8bbac44839466ae2d1f5dd28c1
Tushar S Machale <110717519+TusharSMachale@users.noreply.github.com>
2026-07-22 18:35:10 +0530

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1; // Base case: 1 way to make amount 0 (by choosing no coins)
+
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+
+        return dp[amount];
+    }
+}
