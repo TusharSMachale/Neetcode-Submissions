748f32a85f10017e59f995405059df34f446dcd6
Tushar S Machale <110717519+TusharSMachale@users.noreply.github.com>
2026-08-24 15:41:03 +0530

    Add: reverse-string - submission-2

---
+++ Data Structures & Algorithms/reverse-string/submission-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int n = s.length;
+        int l = 0;
+        int h = n-1;
+
+        while(l < h){
+            char temp = s[l];
+            s[l] = s[h];
+            s[h] = temp;
+            l++;
+            h--;
+        }
+    }
+}
